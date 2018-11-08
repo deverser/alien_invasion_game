aa0573bf0bf1929d648747af15f47312daef342f
deverser <alexmal74@gmail.com>
2018-11-09 00:09:37 +0500

    resized image of ship, class ship was written

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 
 def run_game():
@@ -9,7 +10,8 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-
+    # Создание корабля
+    ship = Ship(screen)
     # Запуск основного цикла игры
     while True:
         # Отслеживание событий клавиатуры и мыши
@@ -17,6 +19,7 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
         # Отображение последнего прорисованного экрана
         pygame.display.flip()
 
