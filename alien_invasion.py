9fa201e9e0d2659aa77b53d880937fa58ddca366
deverser <alexmal74@gmail.com>
2018-11-09 01:48:25 +0500

    refactoring, adding game_functions.py, improved movements of ship, changed background of game screen and ship image from grey to deep blue color.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,7 @@
-import sys
 import pygame
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 
 def run_game():
@@ -11,17 +11,13 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     # Создание корабля
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
     # Запуск основного цикла игры
     while True:
         # Отслеживание событий клавиатуры и мыши
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-        # Отображение последнего прорисованного экрана
-        pygame.display.flip()
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
 
 
 run_game()
--- settings.py
+++ settings.py
@@ -6,4 +6,5 @@ class Settings():
         # Параметры экрана
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (0, 8, 50)
+        self.ship_speed_factor = 1.5
