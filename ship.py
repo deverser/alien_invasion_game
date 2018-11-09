bd14b62fcf09741528c08f62f2c37e0b132a4a34
deverser <alexmal74@gmail.com>
2018-11-10 03:10:54 +0500

    code optimizations in files game_functions and ship

--- ship.py
+++ ship.py
@@ -22,9 +22,9 @@ class Ship():
     def update(self):
         '''Обновляет позицию корабля с учётом флагов'''
         # Обновляется аттрибут center, не rect
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.ai_settings.ship_speed_factor
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.center -= self.ai_settings.ship_speed_factor
         # Обновление аттрибута rect на соновании self.center
         self.rect.centerx = self.center
