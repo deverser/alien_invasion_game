5324fb141a518c5324bd8121c6a0beecac84b23e
deverser <alexmal74@gmail.com>
2018-11-25 03:36:57 +0500

    high score now saves in high_score.json

--- game_functions.py
+++ game_functions.py
@@ -234,4 +234,5 @@ def check_high_score(stats, sb):
     if stats.score > stats.high_score:
         stats.high_score = stats.score
         sb.prep_high_score()
+        stats.save_high_score()
 
--- game_stats.py
+++ game_stats.py
@@ -1,3 +1,6 @@
+import json
+
+
 class GameStats():
     '''Отслеживание статистики для игры'''
     def __init__(self, ai_settings):
@@ -8,7 +11,7 @@ class GameStats():
         self.game_active = False
         self.reset_stats()
         # Рекорд игры не должен сбрасываться
-        self.high_score = 0
+        self.high_score = self.get_high_score()
 
 
     def reset_stats(self):
@@ -16,3 +19,22 @@ class GameStats():
         self.ship_left = self.ai_settings.ship_limit
         self.score = 0
         self.level = 1
+
+    def save_high_score(self):
+        """Сохраняет рекорд игры в файл"""
+        filename = 'high_score.json'
+        with open(filename, 'w') as f:
+            json.dump(self.high_score, f)
+
+    def get_high_score(self):
+        """Извлекает значение рекордных очков игры из файла
+        для дальнейшего использования в программе
+        """
+        filename = 'high_score.json'
+        try:
+            with open(filename) as f:
+                self.high_score = json.load(f)
+        except FileNotFoundError:
+            return 0
+        else:
+            return self.high_score
